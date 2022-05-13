9fae128d8f8a4230a78c14a45f80671941b1ee83
SantoshMThakuri <iamsantoshmthakuri@gmail.com>
2022-05-13 14:36:52 -0400

    done

--- subscribe.java
+++ subscribe.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 class inputprogram {
-    public static void main(String[]args){
-    Scanner input = new Scanner(System.in);
+    public static void main(String[]args)
+    {
+    Scanner scan = new Scanner(System.in);
     
     System.out.print("Enter your name:");
-    String firstname = input.next();
+    String firstname = scan.nextLine();
     
-
     System.out.print("Enter your last name: ");
-    String Lastname = input.next();
+    String Lastname = scan.nextLine();
+    
+    System.out.println("Your full name is:" + firstname + " " + Lastname);
+    
    
-    System.out.println("Your full name is " + firstname + " " + Lastname );
+    
   }
 }
