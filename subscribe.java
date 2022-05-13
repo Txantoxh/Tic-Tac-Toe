b1ab95e60d964664463e31dde434133285e18889
SantoshMThakuri <iamsantoshmthakuri@gmail.com>
2022-05-13 14:08:04 -0400

    name prinitng

--- subscribe.java
+++ subscribe.java
@@ -1,7 +1,16 @@
-class subscribe {
+import java.util.Scanner;
+
+class inputprogram {
+    public static void main(String[]args){
+    Scanner input = new Scanner (System.in);
+    System.out.print("Enter your name:");
+    String firstname = input.next();
+
+    System.out.print("Enter your last name: ");
+    Scanner input = new Scanner (System.in);
+    String Lastname = input.next();
+
+    System.out.println("Your full name is" + firstname + Lastname);
     
-    public static void main(String[] args) {
-           System.out.println("hello world");
-    }
-        
-    }
+   }
+}
